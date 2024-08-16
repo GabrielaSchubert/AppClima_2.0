1a68ba4135b83447c59b36fd70b23e86f62a9da0
sidneckel <sidn.ick2015@gmail.com>
2024-08-16 11:17:02 -0300

    alterando header

--- appclima_2.0/src/App.vue
+++ appclima_2.0/src/App.vue
@@ -5,13 +5,28 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div id="layout">
-    <header>
-      <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'show' }">Show</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">About</RouterLink>
-        </nav>
-      </div>
+      <header>
+    <h2><a href="index.html" class="logo">☁️ ClimaClean</a></h2>
+    <div class="input">
+      <input class="input-cidade"
+        type="text"
+        id="cityinput"
+        placeholder="Digite o nome da cidade que deseja ver o clima:"
+      >
+      <button class="botao-busca" onclick="clicaNoBotao()">
+        <img
+          class="img-busca"
+          alt="buscar"
+          src="https://www.svgrepo.com/show/488200/find.svg"
+        />
+      </button>
+    </div>
+    <nav class="nav">
+      <RouterLink :to="{ name: 'about' }">sobre o app </RouterLink>
+      <RouterLink :to="{ name: 'show' }">Show</RouterLink>
+      <span> | </span>
+      <a href="https://github.com/Jkvua/AppClima" target="_blank">Github do projeto</a>
+    </nav>
     </header>
     <RouterView />
   </div>
@@ -37,5 +52,36 @@ nav a.router-link-exact-active {
 }
 h2 {
   font-size: 20px;
+}
+/* Header */
+header {
+    grid-area: header;
+    background-color: var(--corPrimaria);
+}
+header .logo {
+    text-decoration: none;
+    color: white;
+}
+header:nth-child(1) {
+    text-align: center;
+}
+.img-busca {
+    width: 15px;
+}
+
+@media screen and (min-width: 601px) {
+    header {
+        display: grid;
+        grid-template-columns: 2fr 5fr 2fr;
+        align-items: center;
+    }
+}
+
+@media screen and (max-width: 600px) {
+    header {
+        display: grid;
+        grid-template-rows: 1fr 1fr 1fr;
+        align-items: center;
+    }
 }
 </style>
