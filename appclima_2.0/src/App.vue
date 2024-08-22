9375fa519f05af031d82b0306f89962ad540fed7
sidneckel <sidn.ick2015@gmail.com>
2024-08-22 10:14:05 -0300

    centralizando app

--- appclima_2.0/src/App.vue
+++ appclima_2.0/src/App.vue
@@ -5,38 +5,35 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div id="layout">
-      <header>
-    <h2><a href="index.html" class="logo">☁️ ClimaClean</a></h2>
-    <div class="input">
-      <input class="input-cidade"
-        type="text"
-        id="cityinput"
-        placeholder="Digite o nome da cidade que deseja ver o clima:"
-      >
-      <button class="botao-busca" onclick="clicaNoBotao()">
-        <img
-          class="img-busca"
-          alt="buscar"
-          src="https://www.svgrepo.com/show/488200/find.svg"
-        />
-      </button>
-    </div>
-    <nav class="nav">
-      <RouterLink :to="{ name: 'about' }">sobre o app </RouterLink>
-      <RouterLink :to="{ name: 'show' }">Show</RouterLink>
-      <span> | </span>
-      <a href="https://github.com/Jkvua/AppClima" target="_blank">Github do projeto</a>
-    </nav>
+    <header>
+      <h2><a href="index.html" class="logo">☁️ ClimaClean</a></h2>
+      <div class="input">
+        <input class="input-cidade"
+          type="text"
+          id="cityinput"
+          placeholder="Digite o nome da cidade que deseja ver o clima:"
+        >
+        <button class="botao-busca" @click="clicaNoBotao">
+          <img
+            class="img-busca"
+            alt="buscar"
+            src="https://www.svgrepo.com/show/488200/find.svg"
+          />
+        </button>
+      </div>
+      <nav class="nav">
+        <RouterLink :to="{ name: 'about' }">sobre o app</RouterLink>
+        <RouterLink :to="{ name: 'show' }">Show</RouterLink>
+        <span> | </span>
+        <a href="https://github.com/Jkvua/AppClima" target="_blank">Github do projeto</a>
+      </nav>
     </header>
     <RouterView />
-  </div>  <footer>
-    <p>
-      ☁️ ClimaClean
-    </p>
-    <P>
-      Versão 2.0/2024
-    </P>
-  </footer>
+    <footer>
+      <p>☁️ ClimaClean</p>
+      <p>Versão 1.0/2024</p>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
@@ -46,58 +43,99 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  
-}
-nav {
-  padding: 30px;
-}
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-h2 {
-  font-size: 20px;
-}
-/* Header */
-header {
-    grid-area: header;
-    background-color: var(--corPrimaria);
-}
-header .logo {
-    text-decoration: none;
-    color: white;
-}
-header:nth-child(1) {
-    text-align: center;
-}
-.img-busca {
-    width: 15px;
 }
 
+/* Cabeçalho */
+header {
+  background-color: var(--corPrimaria);
+  display: flex;
+  justify-content: space-between; /* Alinha logo e navegação */
+  align-items: center;
+  padding: 10px 20px;
+}
+
+header .logo {
+  text-decoration: none;
+  color: white;
+  font-size: 24px;
+}
+
+/* Formulário de Busca */
+.input {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.input-cidade {
+  padding: 8px;
+  margin-right: 10px;
+}
+
+.botao-busca {
+  background-color: transparent;
+  border: none;
+}
+
+.img-busca {
+  width: 15px;
+}
+
+/* Navegação */
+.nav {
+  display: flex;
+  align-items: center;
+  gap: 10px; /* Espaçamento entre os itens */
+}
+
+.nav a, .nav span {
+  font-weight: bold;
+  color: rgb(5, 11, 48);
+  text-decoration: none;
+}
+
+.nav a.router-link-exact-active {
+  color: #2c1580;
+}
+
+/* Footer */
+footer {
+  background-color: var(--corPrimaria);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 15px 0;
+  color: rgb(6, 24, 100);
+  text-align: center;
+}
+
+footer p {
+  margin: 5px 0;
+}
+
+/* Responsividade */
 @media screen and (min-width: 601px) {
-    header {
-        display: grid;
-        grid-template-columns: 2fr 5fr 2fr;
-        align-items: center;
-    }
+  header {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+  }
+
+  .input {
+    justify-content: center;
+  }
 }
 
 @media screen and (max-width: 600px) {
-    header {
-        display: grid;
-        grid-template-rows: 1fr 1fr 1fr;
-        align-items: center;
-    }
-}
-/* Footer */
-footer {
-    grid-area: footer;
-    background-color: var(--corPrimaria);
-    width: 100%;
-    display: flex;
-    justify-content: space-around;
+  header {
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+    align-items: center;
+  }
+
+  .input {
+    justify-content: center;
+  }
 }
 </style>
