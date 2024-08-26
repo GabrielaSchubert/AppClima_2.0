1e389ec7977cf3a581f413182595218d416f5fd8
victtows <josevictorjustinosilva@gmail.com>
2024-08-26 12:46:09 -0300

    Atualizaçao na Pagina AboutView

--- appclima_2.0/src/views/AboutView.vue
+++ appclima_2.0/src/views/AboutView.vue
@@ -31,12 +31,12 @@
       display: flex;
       justify-content: center;
       align-items: center;
-      height: 100vh; /* Altura completa da tela */
+      height: 100vh; 
       text-align: center;
     }
   
     .container {
-      max-width: 600px; /* Largura máxima do conteúdo */
+      max-width: 600px;
       margin: 0 auto;
     }
   </style>
