30765a4417110d2690e73b2798b4ae143b7d2370
victtows <josevictorjustinosilva@gmail.com>
2024-08-26 12:45:02 -0300

    Atualizaçao na Pagina AboutView

--- appclima_2.0/src/views/AboutView.vue
+++ appclima_2.0/src/views/AboutView.vue
@@ -5,25 +5,39 @@
 
 <template>
     <main>
-      <h1>Sobre o App</h1>
-      <p>
-            Esse app foi desenvolvido para demonstrar o clima, permitindo que os usuários pesquisem as condições do tempo em diferentes lugares em tempo real. 
+      <div class="container">
+        <h1>Sobre o App</h1>
+        <p>
+          Esse app foi desenvolvido para demonstrar o clima, permitindo que os usuários pesquisem as condições do tempo em diferentes lugares em tempo real.
         </p>
         <p>
-            O app recebeu o nome de ClimaClean, que significa: clima limpo, ou seja, o intuito do site é que os usuários possam ter acesso a informação sobre o clima de forma limpa, sem problemas e sem demora.
+          O app recebeu o nome de ClimaClean, que significa: clima limpo, ou seja, o intuito do site é que os usuários possam ter acesso a informação sobre o clima de forma limpa, sem problemas e sem demora.
         </p>
         <p>
-            Essa é a versão 2.0 do projeto no ano de 2024.
+          Essa é a versão 2.0 do projeto no ano de 2024.
         </p>
         <p>
-            O projeto <strong>ClimaClean</strong> foi desenvolvido por: Camila, Gabriela, José Victor, Otávio e Sidiclei. Alunos do curso Sistemas Da Informação do Instituto Federal Catarinense - IFC.
+          O projeto <strong>ClimaClean</strong> foi desenvolvido por: Camila, Gabriela, José Victor, Otávio e Sidiclei. Alunos do curso Sistemas Da Informação do Instituto Federal Catarinense - IFC.
         </p>
         <p>
-            Saiba mais sobre no <a target="_blank" href="https://github.com/GabrielaSchubert/AppClima_2.0.git">Github do projeto</a>.
+          Saiba mais sobre no <a target="_blank" href="https://github.com/GabrielaSchubert/AppClima_2.0.git">Github do projeto</a>.
         </p>
+      </div>
     </main>
-</template>
-
-<style>
-
-</style>
+  </template>
+  
+  <style>
+    main {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh; /* Altura completa da tela */
+      text-align: center;
+    }
+  
+    .container {
+      max-width: 600px; /* Largura máxima do conteúdo */
+      margin: 0 auto;
+    }
+  </style>
+  
